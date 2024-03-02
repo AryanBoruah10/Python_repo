df860c16fceea63542b3b91ca6d73913dafdc862
Aryan <94703869+AryanBoruah10@users.noreply.github.com>
2024-03-02 19:06:05 +0530

    Create simple_calc.py

---
+++ simple_calc.py
@@ -0,0 +1,12 @@
+A = int(input("Enter first number: "))
+B = int(input("Enter second number: "))
+op = str(input("Enter the operation: "))
+
+if op == "+" :
+    print(A+B)
+elif(op == "-"):
+    print(A-B)
+elif(op == "*"):
+    print(A*B)
+elif(op == "/"):
+    print(A/B)    
